6d2de5a219f794a5bc07ab8e511cbb1eb34988f4
mohanadsyria40 <mohanadsyria40@gmal.com>
2024-10-24 12:46:13 +0300

    Create advanced v-model inding with nested loops
    Create advanced v-model binding with nested loops

--- src/components/MyForm.vue
+++ src/components/MyForm.vue
@@ -1,54 +1,156 @@
 <template>
-  <div class="form">
-    <form action="" @submit.prevent>
-      <div>
-        <label for="">First Name</label>
-        <input type="text" name="firstName" v-model.lazy="student.firstName" />
-        <p>Result is: {{ student.firstName }}</p>
-      </div>
-      <div>
-        <label for="">Last Name</label>
-        <input type="text" name="lastName" v-model.lazy="student.lastName" />
-        <p>Result is: {{ student.lastName }}</p>
-      </div>
-      <div>
-        <label for="">Age</label>
-        <input type="number" name="age" v-model.lazy="student.age" />
-        <p>Result is: {{ student.age }}</p>
-      </div>
-      <div>
-        <label for="">Email</label>
-        <input type="email" name="email" v-model.lazy="student.email" />
-        <p>Result is: {{ student.email }}</p>
-      </div>
-      <div>
-        <label for="">Password</label>
-        <input
-          type="password"
-          name="password"
-          v-model.lazy="student.password"
-        />
-
-        <p>Result is: {{ student.password }}</p>
-      </div>
-      <div class="actions">
-        <input type="submit" value="Register" />
-      </div>
-    </form>
-  </div>
+  <keep-alive>
+    <div class="form">
+      <form action="" @submit.prevent="addMember">
+        <div>
+          <label for="firstName">First Name</label>
+          <input
+            v-model="member.firstName"
+            type="text"
+            name="firstName"
+            id="firstName"
+          />
+        </div>
+        <div>
+          <label for="lastName">Last Name</label>
+          <input
+            v-model="member.lastName"
+            type="text"
+            name="lastName"
+            id="lastName"
+          />
+        </div>
+        <div>
+          <label for="age">Age</label>
+          <input v-model="member.age" type="number" name="age" id="age" />
+        </div>
+        <div>
+          <label for="email">Email</label>
+          <input v-model="member.email" type="email" name="email" id="email" />
+        </div>
+        <div>
+          <label for="password">Password</label>
+          <input
+            v-model="member.password"
+            type="password"
+            name="password"
+            id="password"
+          />
+        </div>
+        <div>
+          <h3>Gender</h3>
+          <label>Male</label>
+          <input
+            v-model="member.gender"
+            type="radio"
+            name="male"
+            id="male"
+            value="Male"
+          />
+          <label>Female</label>
+          <input
+            v-model="member.gender"
+            type="radio"
+            name="female"
+            id="female"
+            value="Female"
+          />
+        </div>
+        <div>
+          <h3>Favorite Books</h3>
+          <label>Ayuhalwalad</label>
+          <input
+            v-model="member.favBooks.ayuhalwalad"
+            type="checkbox"
+            name="ayuhalwalad"
+          />
+          <label>Arbawoon</label>
+          <input
+            v-model="member.favBooks.arbawoon"
+            type="checkbox"
+            name="arbawoon"
+          />
+          <label>Ihya</label>
+          <input v-model="member.favBooks.ihya" type="checkbox" name="ihya" />
+        </div>
+        <div>
+          <h3>Ability</h3>
+          <select v-model="member.ability" name="ability" id="ability">
+            <option value="able">Able</option>
+            <option value="disable">Disable</option>
+          </select>
+        </div>
+        <div>
+          <label for="bio">Bio</label>
+          <textarea v-model="member.bio" name="bio" id="bio" />
+        </div>
+        <div class="actions" style="width: 100%">
+          <input style="width: 50%; margin: auto" type="submit" value="Save" />
+        </div>
+        <div>
+          <ul>
+            <li v-for="(memb, i) in members" :key="i">
+              <p>Name: {{ memb.firstName }} {{ memb.lastName }}</p>
+              <p>Email: {{ memb.email }}</p>
+              <p>Gender: {{ memb.gender }}</p>
+              <p>Age: {{ memb.age }}</p>
+              <p>
+                Favorite books:
+                <strong>
+                  <span v-for="(book, key, i) in memb.favBooks" :key="i">
+                    <span v-if="book">{{ key }}<br /> </span>
+                  </span>
+                </strong>
+              </p>
+              <p>Bio: {{ memb.bio }}</p>
+            </li>
+          </ul>
+        </div>
+      </form>
+    </div>
+  </keep-alive>
 </template>
 <script>
 export default {
   data() {
     return {
-      student: {
+      member: {
         firstName: "",
         lastName: "",
         age: null,
         email: "",
         password: "",
+        gender: "",
+        favBooks: {
+          ayuhalwalad: false,
+          arbawoon: false,
+          ihya: false,
+        },
+        ability: "",
+        bio: "",
       },
+      members: [],
     };
+  },
+  methods: {
+    addMember() {
+      this.members.push(this.member);
+      this.member = {
+        firstName: "",
+        lastName: "",
+        age: null,
+        email: "",
+        password: "",
+        gender: "",
+        favBooks: {
+          ayuhalwalad: false,
+          arbawoon: false,
+          ihya: false,
+        },
+        ability: "",
+        bio: "",
+      };
+    },
   },
 };
 </script>
@@ -58,10 +160,12 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-wrap: wrap;
   padding: 50px;
   background-color: aliceblue;
   width: 50vw;
   margin: auto;
+
   div {
     display: flex;
     align-items: center;
