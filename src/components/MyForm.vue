a0f497acb2aa46395656d84101fbf335e755211a
mohanadsyria40 <mohanadsyria40@gmal.com>
2024-10-24 10:18:21 +0300

    Initiate v-model property for data binding

---
+++ src/components/MyForm.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="form">
+    <form action="" @submit.prevent>
+      <div>
+        <label for="">First Name</label>
+        <input type="text" name="firstName" v-model.lazy="student.firstName" />
+        <p>Result is: {{ student.firstName }}</p>
+      </div>
+      <div>
+        <label for="">Last Name</label>
+        <input type="text" name="lastName" v-model.lazy="student.lastName" />
+        <p>Result is: {{ student.lastName }}</p>
+      </div>
+      <div>
+        <label for="">Age</label>
+        <input type="number" name="age" v-model.lazy="student.age" />
+        <p>Result is: {{ student.age }}</p>
+      </div>
+      <div>
+        <label for="">Email</label>
+        <input type="email" name="email" v-model.lazy="student.email" />
+        <p>Result is: {{ student.email }}</p>
+      </div>
+      <div>
+        <label for="">Password</label>
+        <input
+          type="password"
+          name="password"
+          v-model.lazy="student.password"
+        />
+
+        <p>Result is: {{ student.password }}</p>
+      </div>
+      <div class="actions">
+        <input type="submit" value="Register" />
+      </div>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      student: {
+        firstName: "",
+        lastName: "",
+        age: null,
+        email: "",
+        password: "",
+      },
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.form {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 50px;
+  background-color: aliceblue;
+  width: 50vw;
+  margin: auto;
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 30px;
+    gap: 20px;
+  }
+}
+</style>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,60 +1,19 @@
 <template>
   <div class="main-page">
     <h2>This is my main page</h2>
-    <keep-alive>
-      <my-component :name="name" v-if="!isComponentTwo">
-        <template #slotTitle>
-          <h2>Form Title one</h2>
-        </template>
-        <template #slotInputs>
-          <input type="text" placeholder="Name" />
-          <input type="password" placeholder="Password" />
-        </template>
-        <template #slotAction>
-          <input type="submit" value="Submit" />
-        </template>
-      </my-component>
-    </keep-alive>
-    <hr />
-    <keep-alive>
-      <my-component-two name="Khalid" v-if="isComponentTwo">
-        <template #slotTitle>
-          <h2>Form Title two</h2>
-        </template>
-        <template #slotInputs>
-          <input type="text" placeholder="Name" />
-          <input type="password" placeholder="Password" />
-        </template>
-        <template #slotAction>
-          <input type="submit" value="Submit" />
-        </template>
-      </my-component-two>
-    </keep-alive>
-    <button @click="isComponentTwo = !isComponentTwo">change component</button>
+    <MyForm />
   </div>
 </template>
 
 <script>
-import MyComponent from "@/components/MyComponent.vue";
-import MyComponentTwo from "@/components/MyComponent.vue";
-
+import MyForm from "@/components/MyForm.vue";
 export default {
-  name: "MainPage",
   data() {
-    return {
-      name: "Mohanad",
-      isComponentTwo: false,
-    };
+    return {};
   },
-  methods: {
-    changeActive() {
-      this.isActive = !this.isActive;
-    },
-  },
-
+  methods: {},
   components: {
-    MyComponent,
-    MyComponentTwo,
+    MyForm,
   },
 };
 </script>
