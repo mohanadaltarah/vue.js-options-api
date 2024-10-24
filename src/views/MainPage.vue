c76805df533282248c280124b65e486eda9f0f37
mohanadsyria40 <mohanadsyria40@gmal.com>
2024-10-24 16:31:42 +0300

    Get data from a dummy data Http request

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,20 +1,44 @@
 <template>
   <div class="main-page">
     <h2>This is my main page</h2>
-    <MyForm />
+    <div class="products">
+      <ul>
+        <li v-for="product in products" :key="product.id">
+          <div>
+            <img :src="product.thumbnail" width="100" alt="" />
+          </div>
+          <p>Brand: {{ product.title }}</p>
+          <p>Category: {{ product.category }}</p>
+          <p>Description: {{ product.description }}</p>
+          <p>Price: {{ product.price }}</p>
+          <p>Stock: {{ product.stock }}</p>
+          <div>
+            <hr />
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import MyForm from "@/components/MyForm.vue";
 export default {
   data() {
-    return {};
+    return {
+      products: [],
+    };
   },
-  methods: {},
-  components: {
-    MyForm,
+  methods: {
+    async getProducts() {
+      await fetch("https://dummyjson.com/products")
+        .then((res) => res.json())
+        .then((data) => (this.products = data.products));
+    },
   },
+  async mounted() {
+    await this.getProducts();
+  },
+  components: {},
 };
 </script>
 <style scoped lang="scss">
@@ -23,7 +47,23 @@ export default {
     color: #2c3e50;
   }
   p {
-    color: green;
+    color: black;
+  }
+
+  ul {
+    list-style-type: none;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+  }
+
+  li {
+    display: flex;
+    border-right: 1px;
+    justify-content: space-between;
+    width: 100%;
   }
 }
 </style>
