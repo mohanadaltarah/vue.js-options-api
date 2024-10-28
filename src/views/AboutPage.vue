42706b0db6343409703dc0728378b1bc11dd9e9e
mohanadsyria40 <mohanadsyria40@gmal.com>
2024-10-28 12:28:23 +0300

    Create advanced computed methods

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="main-container">
     <h2>This is the about page</h2>
+    <p>{{ fullName }}</p>
+    <p>{{ calcNums }}</p>
+
     <ul>
-      <li v-for="st in students" :key="st.id">
+      <input type="search" v-model="search" />
+      <li v-for="st in searchSt" :key="st.id">
         <p>Name: {{ st.name }}</p>
         <p>Age: {{ st.age }}</p>
       </li>
@@ -14,5 +18,27 @@ import stMixin from "@/mixins/stMixin";
 
 export default {
   mixins: [stMixin],
+  data() {
+    return {
+      firstName: "Mohanad",
+      lastName: "Altarah",
+      myArr: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      search: "",
+    };
+  },
+  computed: {
+    fullName() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+    calcNums() {
+      return this.myArr.reduce((acc, current) => acc + current);
+    },
+    getOldStudents() {
+      return this.students.filter((st) => st.age >= 14);
+    },
+    searchSt() {
+      return this.students.filter((st) => st.name.match(this.search));
+    },
+  },
 };
 </script>
