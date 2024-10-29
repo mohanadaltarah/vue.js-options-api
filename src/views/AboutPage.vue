0abacb1342401259d9b5602484376fe6f13f0cf4
mohanadsyria40 <mohanadsyria40@gmal.com>
2024-10-29 15:40:51 +0300

    Create watchers for primitive and non-primitive data types

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="about">
     <h1>This is the about page</h1>
-    <input type="text" v-model="username" />
+    <input type="text" v-model="userData.name" />
     <button @click="isActive = !isActive">Change Active</button>
     <h2>Username is: {{ username }}</h2>
     <h2>User active status is: {{ isActive }}</h2>
+    <button @click="myArray.push('new data')">add to arr</button>
   </div>
 </template>
 
@@ -14,6 +15,11 @@ export default {
     return {
       username: "",
       isActive: false,
+      userData: {
+        name: "",
+        age: 24,
+      },
+      myArray: [],
     };
   },
   methods: {
@@ -25,9 +31,15 @@ export default {
     console.log(this.$router.hasRoute("Profile"));
   },
   watch: {
-    username(newValue, oldValue) {
+    "userData.name"(newValue, oldValue) {
       console.log("new value: ", newValue);
       console.log("old val: ", oldValue);
+    },
+    myArray: {
+      handler(newValue) {
+        console.log("new value: ", newValue);
+      },
+      deep: true,
     },
   },
 };
